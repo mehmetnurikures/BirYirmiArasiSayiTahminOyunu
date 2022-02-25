8715df6abd1946d1cb427325a983b15917ba683c
Mehmet Nuri Kureş <mnurikures@hotmail.com>
2022-02-25 14:19:53 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirYirmiArasiSayiTahminOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int i = 1;
+            Random rdn = new Random();
+            int rastgele = rdn.Next(0, 20);
+            while (true)
+            {
+                Console.Write("Bir sayı giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if (sayi==rastgele)
+                {
+                    break;
+                }
+                i++;
+            }
+            Console.WriteLine(i+ " 'nci tahmininizde bilgisayarın atadığı sayıyı buldunuz. Tebrikler!");
+
+            Console.ReadKey();
+        }
+    }
+}
